f4b7ae422830211173daeee327a61d3af9918ae1
Athul Raj <athuljayaraj@gmail.com>
2018-12-21 11:24:58 +0530

    Initial commit

--- src/main/java/com/flytxt/atom/PerformanceTest.java
+++ src/main/java/com/flytxt/atom/PerformanceTest.java
@@ -3,15 +3,16 @@
  */
 package com.flytxt.atom;
 
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 import org.apache.avro.Schema;
-import org.apache.avro.file.DataFileWriter;
-import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumWriter;
-import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.io.DatumWriter;
+import org.apache.avro.io.Encoder;
+import org.apache.avro.io.EncoderFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.flytxt.atom.entity.Complex;
 
@@ -21,6 +22,8 @@ import com.flytxt.atom.entity.Complex;
  */
 public class PerformanceTest {
 
+	public static Logger logger = LoggerFactory.getLogger(PerformanceTest.class);
+
 	/**
 	 * @throws IOException
 	 * 
@@ -29,28 +32,32 @@ public class PerformanceTest {
 
 		Complex complex = new Complex(true);
 		// Instantiating the Schema.Parser class.
-		Schema schema = new Schema.Parser().parse("");
+		Schema schema = new Schema.Parser().parse(
+				"{\"type\":\"record\",\"name\":\"Complex\",\"namespace\":\"com.flytxt.atom.entity\",\"fields\":[{\"name\":\"longArray\",\"type\":[{\"type\":\"array\",\"items\":[\"long\",\"null\"],\"java-class\":\"[Ljava.lang.Long;\"},\"null\"]},{\"name\":\"boolArray\",\"type\":[{\"type\":\"array\",\"items\":[\"boolean\",\"null\"],\"java-class\":\"[Ljava.lang.Boolean;\"},\"null\"]}]}");
 		// Instantiating the GenericRecord class.
-		GenericRecord e1 = new GenericData.Record(schema);
-		// Insert data according to schema
-		e1.put("name", "ramu");
-		e1.put("id", 001);
-		e1.put("salary", 30000);
-		e1.put("age", 25);
-		e1.put("address", "chenni");
-		GenericRecord e2 = new GenericData.Record(schema);
-		e2.put("name", "rahman");
-		e2.put("id", 002);
-		e2.put("salary", 35000);
-		e2.put("age", 30);
-		e2.put("address", "Delhi");
-		DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<GenericRecord>(schema);
-		DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<GenericRecord>(datumWriter);
-		dataFileWriter.create(schema, new File("/home/hduser/Desktop/AVRO/mydata.txt"));
-		dataFileWriter.append(e1);
-		dataFileWriter.append(e2);
-		dataFileWriter.close();
-		System.out.println("data successfully serialized");
+		byte[] serealizeAvroHttpRequestJSON = serealizeAvroHttpRequestJSON(complex, schema);
+
+		System.out.println(serealizeAvroHttpRequestJSON.length);
 	}
 
+	public static byte[] serealizeAvroHttpRequestJSON(Complex request, Schema schema) {
+
+		DatumWriter<Complex> writer = new GenericDatumWriter<>(schema);
+		byte[] data = new byte[0];
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
+		Encoder jsonEncoder = null;
+		try {
+			jsonEncoder = EncoderFactory.get().jsonEncoder(schema, stream);
+			writer.write(request, jsonEncoder);
+			jsonEncoder.flush();
+			data = stream.toByteArray();
+		} catch (IOException e) {
+			logger.error("Serialization error:" + e.getMessage());
+		}
+		return data;
+	}
+
+	public static void main(String[] args) throws IOException {
+		serializer();
+	}
 }
